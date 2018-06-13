6febf23856f2ad84fd3e9713df4d5859e34ae2a3
MadhuraReddy <madhura2419@gmail.com>
2018-06-13 17:56:12 +0530

    toast

--- app/src/main/java/com/example/wave/hellogit/MainActivity.java
+++ app/src/main/java/com/example/wave/hellogit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hello Git", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello y77jiubuinm", Toast.LENGTH_SHORT).show();
     }
 }
